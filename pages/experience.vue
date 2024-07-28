8a0ab25091e565c2f7227cb528315ece294a8d74
Jo <jojostev@gmail.com>
2024-07-28 22:02:35 +0400

    added experience page

---
+++ pages/experience.vue
@@ -0,0 +1,7 @@
+<template>
+    <UContainer>
+        <div class="w-full pt-12 md:pt-24 lg:pt-32 flex-row pb-10">
+            <h1>Experience Page</h1>
+        </div>
+    </UContainer>
+</template>
