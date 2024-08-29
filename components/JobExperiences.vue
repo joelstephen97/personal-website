f696698a0a9795c0877573c253f6f3910bec5756
Jo <jojostev@gmail.com>
2024-08-29 19:01:02 +0400

    remove colors so that dark light mode works

--- components/JobExperiences.vue
+++ components/JobExperiences.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="p-3 border border-red-100 rounded-2xl drop-shadow-2xl mb-4">
       <h3 class="text-2xl font-bold mb-1">{{ title }}</h3>
-      <p class="text-black-600 font-bold">{{ company }} - {{ location }}</p>
-      <p class="text-gray-800">{{ duration }}</p>
+      <p class="font-bold">{{ company }} - {{ location }}</p>
+      <p class="">{{ duration }}</p>
       <ul class="mt-3">
         <li v-for="task in tasks" :key="task" class="-mt-1">
           - {{ task }}
         </li>
       </ul>
-      <p class="mt-2 text-black"><strong>Skills:</strong> {{ skills.join(', ') }}</p>
+      <p class="mt-2"><strong>Skills:</strong> {{ skills.join(', ') }}</p>
     </div>
   </template>
   
