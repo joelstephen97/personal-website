d357089771ce313d7a7bacb1add5797f5b42584c
Jo <jojostev@gmail.com>
2024-10-10 18:08:25 +0400

    background and some ui changes

--- pages/project/rainbow-6-randomizer.vue
+++ pages/project/rainbow-6-randomizer.vue
@@ -1,72 +1,87 @@
 <template>
-  <div class="flex flex-col items-center p-4 w-full h-full">
-    <div class="flex flex-row justify-between items-center w-full max-w-4xl mb-8">
-      <h1 class="text-2xl font-bold uppercase">Rainbow 6 Siege Operator Randomizer</h1>
-      <UButton @click="toggleColorMode" color="white" class="p-2 rounded-full drop-shadow-sm">
-        <div class="flex justify-center items-center font-bold">
-          <UIcon :name="colorMode.value === 'dark' ? 'i-heroicons-sun' : 'i-heroicons-moon'" class="w-6 h-6 mr-2" :class="colorMode.value === 'dark' ? 'text-white' : 'text-black'" />
-          {{ colorMode.value === 'dark' ? 'Light' : 'Dark' }}
-        </div>
-      </UButton>
-    </div>
+  <div
+    :style="{ backgroundImage: `url(${currentBackground})` }"
+    class="relative flex flex-col items-center p-4 w-screen h-screen bg-cover bg-repeat bg-center transition-all duration-500"
+  >
+    <!-- Overlay Div -->
+    <div
+      :class="colorMode.value === 'dark' ? 'bg-black opacity-50' : 'bg-white opacity-50'"
+      class="fixed inset-0"
+      aria-hidden="true"
+    ></div>
 
-    <!-- Number of Players Selection -->
-    <div class="w-full max-w-4xl flex flex-row justify-between items-center mb-8">
-      <p class="text-lg font-semibold w-1/2">Select Number of Players:</p>
-      
-      <USelect v-model="numberOfPlayers" :options="stacking" option-attribute="name" class="w-full">
-        </USelect>
-    </div>
-
-    <!-- Player Names Input Section -->
-    <div class="w-full max-w-4xl flex flex-col mb-8">
-      <p class="text-lg font-semibold mb-2">Enter Player Names for Random Characters:</p>
-      <div class="grid grid-cols-1 md:grid-cols-5 gap-4 w-full items-center">
-        <input
-          v-for="(name, index) in playerNames.slice(0, numberOfPlayers)"
-          :key="index"
-          v-model="playerNames[index]"
-          class="border p-2 rounded w-full"
-          :placeholder="'Player ' + (index + 1)"
-        />
+    <!-- Content Container -->
+    <div class="relative z-10 flex flex-col items-center w-full max-w-4xl">
+      <!-- Header -->
+      <div class="flex flex-row justify-between items-center w-full mb-8">
+        <h1 class="text-2xl font-bold uppercase">Rainbow 6 Siege Operator Randomizer</h1>
+        <UButton @click="toggleColorMode" color="white" class="p-2 rounded-full drop-shadow-sm">
+          <div class="flex justify-center items-center font-bold">
+            <UIcon :name="colorMode.value === 'dark' ? 'i-heroicons-sun' : 'i-heroicons-moon'" class="w-6 h-6 mr-2" :class="colorMode.value === 'dark' ? 'text-white' : 'text-black'" />
+            {{ colorMode.value === 'dark' ? 'Light' : 'Dark' }}
+          </div>
+        </UButton>
       </div>
-    </div>
 
-    <!-- Attackers Section -->
-    <div class="w-full max-w-4xl flex flex-col items-center mb-8">
-      <UButton icon="i-heroicons-arrow-path" color="black" @click="randomizeAttackers" class="mb-4 uppercase font-bold text-lg">Randomize {{ numberOfPlayers }} Attackers</UButton>
-      <div v-if="selectedAttackers.length" class="w-full">
-        <p class="text-lg font-semibold mb-2">Selected Attackers:</p>
-        <div :class="`grid grid-cols-1 md:grid-cols-${numberOfPlayers} gap-4`">
-          <div
-            class="flex flex-col items-center p-2 rounded"
-            v-for="(assignment, index) in selectedAttackers"
-            :key="assignment.operator.name"
-          >
-            <img :src="assignment.operator.image" :alt="assignment.operator.name" class="w-20 h-20 object-cover mb-2" />
-            <p class="text-center font-medium mb-1">{{ assignment.operator.name }}</p>
-            <p class="text-center text-sm mb-1">Assigned to: {{ assignment.name }}</p>
-            <button @click="rerandomizeAttacker(index)" class="text-blue-500 hover:underline">Re-randomize</button>
+      <!-- Number of Players Selection -->
+      <div class="flex flex-row justify-between items-center mb-8 w-full">
+        <p class="text-lg font-semibold w-1/2">Select Number of Players:</p>
+        <USelect v-model="numberOfPlayers" :options="stacking" option-attribute="name" class="w-full">
+        </USelect>
+      </div>
+
+      <!-- Player Names Input Section -->
+      <div class="flex flex-col mb-8 w-full">
+        <p class="text-lg font-semibold mb-2">Enter Player Names for Random Characters:</p>
+        <div class="grid grid-cols-1 md:grid-cols-5 gap-4 w-full items-center">
+          <input
+            v-for="(name, index) in playerNames.slice(0, numberOfPlayers)"
+            :key="index"
+            v-model="playerNames[index]"
+            class="border p-2 rounded w-full"
+            :placeholder="'Player ' + (index + 1)"
+          />
+        </div>
+      </div>
+
+      <!-- Attackers Section -->
+      <div class="flex flex-col items-center mb-8 w-full">
+        <UButton icon="i-heroicons-arrow-path" color="black" @click="randomizeAttackers" class="mb-4 uppercase font-bold text-lg">Randomize {{ numberOfPlayers }} Attackers</UButton>
+        <div v-if="selectedAttackers.length" class="w-full">
+          <p class="text-lg font-bold mb-2 uppercase">Selected Attackers:</p>
+          <div class=" p-2 flex flex-row flex-nowrap items-center justify-evenly gap-4 w-[fit] overflow-x-auto">
+            <div
+              class="flex flex-col items-center p-2 rounded max-h-fit"
+              :class="colorMode.value === 'dark' ? 'bg-gray-900 shadow-[0_0_10px_white]' : ' bg-slate-100 shadow-[0_0_10px_black]'"
+              v-for="(assignment, index) in selectedAttackers"
+              :key="assignment.operator.name"
+            >
+              <img :src="assignment.operator.image" :alt="assignment.operator.name" class="w-20 h-20 object-cover mb-2" />
+              <p class="text-center font-bold mb-1 uppercase">{{ assignment.operator.name }}</p>
+              <p class="text-center font-bold text-sm mb-1 text-ellipsis overflow-hidden w-28">{{ assignment.name }}</p>
+              <button @click="rerandomizeAttacker(index)" class="text-blue-500 hover:underline text-md">Re-randomize</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- Defenders Section -->
-    <div class="w-full max-w-4xl flex flex-col items-center">
-      <UButton icon="i-heroicons-arrow-path" color="black" @click="randomizeDefenders" class="mb-4 uppercase font-bold text-lg">Randomize {{ numberOfPlayers }} Defenders</UButton>
-      <div v-if="selectedDefenders.length" class="w-full">
-        <p class="text-lg font-semibold mb-2">Selected Defenders:</p>
-        <div :class="`grid grid-cols-1 md:grid-cols-${numberOfPlayers} gap-4`">
-          <div
-            class="flex flex-col items-center p-2 rounded"
-            v-for="(assignment, index) in selectedDefenders"
-            :key="assignment.operator.name"
-          >
-            <img :src="assignment.operator.image" :alt="assignment.operator.name" class="w-20 h-20 object-cover mb-2" />
-            <p class="text-center font-medium mb-1">{{ assignment.operator.name }}</p>
-            <p class="text-center text-sm mb-1">Assigned to: {{ assignment.name }}</p>
-            <button @click="rerandomizeDefender(index)" class="text-blue-500 hover:underline">Re-randomize</button>
+      <!-- Defenders Section -->
+      <div class="flex flex-col items-center w-full">
+        <UButton icon="i-heroicons-arrow-path" color="black" @click="randomizeDefenders" class="mb-4 uppercase font-bold text-lg">Randomize {{ numberOfPlayers }} Defenders</UButton>
+        <div v-if="selectedDefenders.length" class="w-full">
+          <p class="text-lg font-bold mb-2 uppercase">Selected Defenders:</p>
+          <div class=" p-2 flex flex-row flex-nowrap items-center justify-evenly gap-4 w-[fit] overflow-x-auto">
+            <div
+              class="flex flex-col items-center p-2 rounded"
+              :class="colorMode.value === 'dark' ? 'bg-gray-900 shadow-[0_0_10px_white]' : ' bg-slate-100 shadow-[0_0_10px_black]'"
+              v-for="(assignment, index) in selectedDefenders"
+              :key="assignment.operator.name"
+            >
+              <img :src="assignment.operator.image" :alt="assignment.operator.name" class="w-20 h-20 object-cover mb-2" />
+              <p class="text-center font-bold mb-1 uppercase">{{ assignment.operator.name }}</p>
+              <p class="text-center font-bold text-sm mb-1 text-ellipsis overflow-hidden w-28">{{ assignment.name }}</p>
+              <button @click="rerandomizeDefender(index)" class="text-blue-500 hover:underline text-md">Re-randomize</button>
+            </div>
           </div>
         </div>
       </div>
@@ -75,9 +90,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 
+// SEO and Page Meta
 useSeoMeta({
   title: 'Rainbow Six Siege Operator Randomizer',
   description: 'Randomly select operators for Rainbow Six Siege. Choose attackers or defenders with the option to re-randomize individual selections.',
@@ -159,7 +175,6 @@ const attackers = createOperators(attackerNames, specialAttackers);
 const defenders = createOperators(defenderNames, specialDefenders);
 
 const playerNames = useLocalStorage<string[]>('player-names', ['', '', '', '', '']);
-
 const numberOfPlayers = useLocalStorage<number>('number-of-players', 5);
 
 watch(numberOfPlayers, (newVal) => {
@@ -213,6 +228,7 @@ function randomizeDefenders() {
   randomizeOperators(defenders, selectedDefenders);
 }
 
+// Function to Rerandomize a Specific Operator
 function rerandomizeOperator(index: number, operatorList: Operator[], selectedOperators: typeof selectedAttackers) {
   const alreadySelected = selectedOperators.value.map((a) => a.operator.name);
   const availableOperators = operatorList.filter((op) => !alreadySelected.includes(op.name));
@@ -228,6 +244,7 @@ function rerandomizeDefender(index: number) {
   rerandomizeOperator(index, defenders, selectedDefenders);
 }
 
+// Function to Get a Secure Random Integer
 function getSecureRandomInt(max: number) {
   const array = new Uint32Array(1);
   let rand;
@@ -238,6 +255,7 @@ function getSecureRandomInt(max: number) {
   return rand % max;
 }
 
+// Function to Shuffle and Get Random Operators
 function getRandomOperators(operatorList: Operator[], count: number) {
   const operators = [...operatorList];
   for (let i = operators.length - 1; i > 0; i--) {
@@ -247,8 +265,60 @@ function getRandomOperators(operatorList: Operator[], count: number) {
   return operators.slice(0, count);
 }
 
+// Color Mode Toggle
 const colorMode = useColorMode();
 const toggleColorMode = () => {
   colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
 };
+
+// --- Rotating Background Implementation ---
+const backgrounds = [
+  'https://wallpapercave.com/wp/wp1846902.jpg',
+  'https://wallpapercave.com/wp/wp1846947.jpg',
+  'https://wallpapercave.com/wp/wp1847065.jpg',
+  'https://wallpapercave.com/wp/wp1847071.jpg',
+  'https://wallpapercave.com/wp/wp1847070.jpg',
+  'https://wallpapercave.com/wp/wp1847033.jpg',
+  'https://wallpapercave.com/wp/wp2125791.jpg',
+  'https://wallpapercave.com/wp/wp2125809.jpg',
+  'https://wallpapercave.com/wp/wp2125895.jpg',
+  'https://wallpapercave.com/wp/wp2125851.jpg',
+  'https://wallpapercave.com/wp/wp2125877.jpg',
+  'https://wallpapercave.com/wp/wp1980806.jpg',
+  'https://wallpapercave.com/wp/wp1846984.jpg',
+  'https://wallpapercave.com/wp/wp1980779.jpg',
+];
+
+const currentBackground = ref('');
+
+function selectRandomBackground() {
+  const randomIndex = getSecureRandomInt(backgrounds.length);
+  currentBackground.value = backgrounds[randomIndex];
+}
+
+let backgroundInterval: number | undefined;
+
+function startBackgroundRotation() {
+  backgroundInterval = window.setInterval(() => {
+    selectRandomBackground();
+  }, 300000);
+}
+
+onMounted(() => {
+  selectRandomBackground();
+  startBackgroundRotation();
+});
+
+onBeforeUnmount(() => {
+  if (backgroundInterval !== undefined) {
+    clearInterval(backgroundInterval);
+  }
+});
 </script>
+
+<style scoped>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
