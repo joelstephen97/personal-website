01ec4e95bbffb1e31db6d7354cde52504ad0771d
Joel Stephen <jojostev@gmail.com>
2025-05-03 21:15:15 +0400

    changes to project component

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,62 +1,60 @@
 <template>
   <UContainer>
-    <div class="w-full pt-12 md:pt-24 lg:pt-32 flex-row pb-10">
-      <div
-        class="flex flex-col flex-wrap sm:flex-row gap-4 justify-center items-center"
+    <!-- Section Header -->
+    <div class="w-full pt-10 md:pt-24 lg:pt-32 flex-row pb-10">
+      <h1
+        class="lg:leading-tighter pb-2 text-4xl font-bold tracking-tighter sm:text-2xl md:text-4xl xl:text-6xl 2xl:text-6xl -mt-8 md:-mt-20"
       >
-        <div class="w-full sm:w-1/3">
-          <UCard :ui="{ background: 'bg-white dark:bg-black' }">
-            <template #header>
-              <div
-                class="flex flex-row flex-nowrap items-center justify-center text-center uppercase font-bold text-xl"
-              >
-                Css Experiments
-              </div>
-            </template>
+        <TypingEffect :strings="['PROJECTS']" :loop="false" />
+      </h1>
+      <p class="text-lg text-gray-800 dark:text-gray-200 mt-4">
+        A showcase of my latest experiments and fun utilities. Dive in and
+        explore!
+      </p>
+    </div>
+    <!-- Projects Grid -->
+    <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <ProjectCard
+        v-for="proj in projects"
+        :key="proj.id"
+        :title="proj.title"
+        :description="proj.description"
+        :link="proj.link"
+        :button-text="proj.buttonText"
+      />
 
-            My Css Doodles will go here
-
-            <template #footer>
-              <NuxtLink to="/project/templates">
-                <UButton class="uppercase" color="black">
-                  Check it out
-                  <template #trailing>
-                    <UIcon
-                      name="i-heroicons-arrow-right-20-solid"
-                      class="w-5 h-5"
-                    />
-                  </template>
-                </UButton>
-              </NuxtLink>
-            </template>
-          </UCard>
-        </div>
-        <div class="w-full sm:w-1/3">
-          <UCard :ui="{ background: 'bg-white dark:bg-black' }">
-            <template #header>
-              <div
-                class="flex flex-row flex-nowrap items-center justify-center text-center uppercase font-bold text-xl"
-              >
-                Rainbow 6 Randomizer
-              </div>
-            </template>
-            Testing some frontend randomizer code
-            <template #footer>
-              <NuxtLink to="/project/rainbow-6-randomizer">
-                <UButton class="uppercase" color="black">
-                  Check it out
-                  <template #trailing>
-                    <UIcon
-                      name="i-heroicons-arrow-right-20-solid"
-                      class="w-5 h-5"
-                    />
-                  </template>
-                </UButton>
-              </NuxtLink>
-            </template>
-          </UCard>
-        </div>
+      <!-- Placeholder for Future Projects -->
+      <div>
+        <UCard
+          :ui="{ background: 'bg-white dark:bg-black' }"
+          class="rounded-xl p-6 shadow-md border border-gray-200 dark:border-gray-800 h-full flex items-center justify-center"
+        >
+          <div class="text-center text-gray-500 dark:text-gray-400">
+            More coming soon...
+          </div>
+        </UCard>
       </div>
     </div>
   </UContainer>
 </template>
+
+<script setup lang="ts">
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+  link: string;
+  buttonText?: string;
+}
+
+const projects: Project[] = [
+  {
+    id: 1,
+    title: "Rainbow 6 Randomizer",
+    description:
+      "Randomize your operator picks for Rainbow Six Siege with a few clicks.",
+    link: "/project/rainbow-6-randomizer",
+    buttonText: "Try It Out",
+  },
+];
+</script>
